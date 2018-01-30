4a3ef0b2737fa36df5b1d72e9469933f07302c19
Bharat Agarwal <bharatagarwal@gmail.com>
2018-01-30 11:50:50 +0530

    Bringing back solutions from LTP

---
+++ pine-book/07_deaf-grandma.rb
@@ -0,0 +1,24 @@
+bye_count = 0
+
+while true
+
+	print "Say something: "
+	said = gets.chomp
+
+	if said == 'BYE'
+		bye_count = bye_count + 1
+	else
+		bye_count = 0
+	end
+
+	if bye_count >=3
+		break
+	end
+
+	if said == said.upcase
+		year = 1930 + rand(21)
+		puts "NO, NOT SINCE " + year.to_s + "!"
+	else
+		puts "HUH! SPEAK UP, SONNY!"
+	end
+end
