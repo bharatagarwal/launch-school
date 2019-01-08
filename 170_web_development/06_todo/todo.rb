b73a4919dd101f3d50a59782c6470997bdcfab59
Bharat Agarwal <bharatagarwal@gmail.com>
2019-01-08 16:01:59 +0530

    back to todo base

--- 170_web_development/06_todo/todo.rb
+++ 170_web_development/06_todo/todo.rb
@@ -2,30 +2,6 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
-configure do
-  enable :sessions
-  set :session_secret, 'secret'
-end
-
-before do
-  session[:lists] ||= []
-end
-
-get '/lists' do
-  @lists = session[:lists]
-#    { name: "Lunch Groceries",
-#      todos: [] },
-#    { name: "Dinner Groceries",
-#      todos: [] }
-#  ]
-  erb :lists, layout: :layout
-end
-
 get '/' do
-  redirect '/lists'
-end
-
-get '/lists/new' do
-  
-  erb :new_list, layout: :layout
+  erb "You have no lists.", layout: :layout
 end
