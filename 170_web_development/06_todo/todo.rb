8369bb1701b0a8dc0d472fd288dbc51930480a64
Bharat Agarwal <bharatagarwal@gmail.com>
2019-01-08 15:30:13 +0530

    Starting todos in 170

--- 170_web_development/06_todo/todo.rb
+++ 170_web_development/06_todo/todo.rb
@@ -1,15 +1,31 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get '/lists' do
+  @lists = session[:lists]
+#    { name: "Lunch Groceries",
+#      todos: [] },
+#    { name: "Dinner Groceries",
+#      todos: [] }
+#  ]
+  erb :lists, layout: :layout
+end
 
 get '/' do
   redirect '/lists'
 end
 
-get "/lists" do
-  @lists = [
-    {name: "Lunch Groceries", todos: []},
-    {name: "Dinner Groceries", todos: []}
-  ]
-  erb :lists, layout: :layout
+get '/lists/new' do
+  
+  erb :new_list, layout: :layout
 end
