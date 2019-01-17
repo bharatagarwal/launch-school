b0e34603f15c7e85e4fb91f440c80783300d6631
Bharat Agarwal <bharatagarwal@gmail.com>
2019-01-17 13:10:17 +0530

    File index page added

--- 170_web_development/09_cms/cms.rb
+++ 170_web_development/09_cms/cms.rb
@@ -2,6 +2,12 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
+root = __FILE__.delete_suffix(File.basename(__FILE__))
+
 get '/' do
-  "Getting started."
+  @files = Dir.glob("#{root}/data/*")
+              .map { |path| File.basename(path) }
+              .sort
+
+  erb :index
 end
