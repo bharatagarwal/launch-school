9e97a93380566fe833e92942643b41a66d239ee7
Bharat Agarwal <bharatagarwal@gmail.com>
2019-02-19 12:30:05 +0530

    cms: checking for non existent documents

--- 175_networked_applications/08_cms/cms.rb
+++ 175_networked_applications/08_cms/cms.rb
@@ -2,14 +2,24 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 
-get '/' do
-  # binding.pry
-  @filenames = Dir.children('data')
+configure do
+  enable :sessions
+end
 
+before do
+  @filenames = Dir.children('data')
+end
+
+get '/' do
   erb :files
 end
 
 get '/:filename' do
+  if !@filenames.include?(params[:filename])
+    session[:error] = "#{params[:filename]} does not exist."
+    redirect '/'
+  end
+  
   headers["Content-Type"] = "text/plain"
    
   File.read("./data/#{params[:filename]}")
