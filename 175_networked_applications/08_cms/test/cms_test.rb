7347408803826c5e8bdb963ea8ce209cb84017e0
Bharat Agarwal <bharatagarwal@gmail.com>
2019-02-19 12:33:24 +0530

    cms: test for non-existent file

--- 175_networked_applications/08_cms/test/cms_test.rb
+++ 175_networked_applications/08_cms/test/cms_test.rb
@@ -26,4 +26,16 @@ class CMSTest < Minitest::Test
     assert_equal "text/plain", last_response["Content-Type"]
     assert_includes last_response.body, "-"
   end
+
+  def test_document_not_found
+    get '/notafile.ext'
+    assert_equal 302, last_response.status
+
+    get last_response["Location"]
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "notafile.ext does not exist"
+
+    get '/'
+    refute_includes last_response.body, "notafile.ext does not exist"
+  end
 end
