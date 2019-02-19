4113f85f55365f811b329955c803d1208bac730a
Bharat Agarwal <bharatagarwal@gmail.com>
2019-02-19 12:56:10 +0530

    cms: rendering markdown files

--- 175_networked_applications/08_cms/cms.rb
+++ 175_networked_applications/08_cms/cms.rb
@@ -10,6 +10,11 @@ before do
   @filenames = Dir.children('data')
 end
 
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
 get '/' do
   erb :files
 end
@@ -19,8 +24,15 @@ get '/:filename' do
     session[:error] = "#{params[:filename]} does not exist."
     redirect '/'
   end
-  
-  headers["Content-Type"] = "text/plain"
-   
-  File.read("./data/#{params[:filename]}")
+
+  file_contents = File.read("./data/#{params[:filename]}")
+
+  extension = params[:filename].split('.')[1]
+
+  if extension == 'md'
+    return render_markdown(file_contents)
+  else
+    headers["Content-Type"] = "text/plain" 
+    File.read("./data/#{params[:filename]}")
+  end
 end
--- 175_networked_applications/08_cms/test/cms_test.rb
+++ 175_networked_applications/08_cms/test/cms_test.rb
@@ -15,13 +15,13 @@ class CMSTest < Minitest::Test
     get '/'
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    assert_includes last_response.body, "about.txt"
+    assert_includes last_response.body, "about.md"
     assert_includes last_response.body, "changes.txt"
     assert_includes last_response.body, "history.txt"
   end
 
   def test_file
-    get '/about.txt'
+    get '/changes.txt'
     assert_equal 200, last_response.status
     assert_equal "text/plain", last_response["Content-Type"]
     assert_includes last_response.body, "-"
@@ -37,5 +37,13 @@ class CMSTest < Minitest::Test
 
     get '/'
     refute_includes last_response.body, "notafile.ext does not exist"
+  end
+
+  def test_viewing_markdown_document
+    get '/about.md'
+    
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]  
+    assert_includes last_response.body, "<h2>"
   end
 end
