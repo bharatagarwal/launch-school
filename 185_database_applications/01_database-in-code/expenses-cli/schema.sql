52ff39d9a0e86ae20976563cfac7f6af37384d6e
Bharat Agarwal <bharatagarwal@gmail.com>
2019-04-03 10:29:57 +0530

    expenses-cli: added default help + list display

---
+++ 185_database_applications/01_database-in-code/expenses-cli/schema.sql
@@ -0,0 +1,14 @@
+-- when the expense was created
+-- how much the expense was
+-- what the expense was for
+
+create table expenses (
+  id serial primary key,
+  amount numeric(6, 2) not null,
+  memo text not null,
+  created_on date not null
+)
+
+alter table expenses
+add constrain positive_amount
+check (amount > 0.00);
