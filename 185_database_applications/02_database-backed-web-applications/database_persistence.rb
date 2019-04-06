c75e9a399fce004ae37228d9dddefab7b48e0e47
Bharat Agarwal <bharatagarwal@gmail.com>
2019-04-06 16:13:27 +0530

    todos: code cleanup + addressed n+1 queries

--- 185_database_applications/02_database-backed-web-applications/database_persistence.rb
+++ 185_database_applications/02_database-backed-web-applications/database_persistence.rb
@@ -16,45 +16,54 @@ class DatabasePersistence
   end
 
   def find_list(id)
-    sql = "select * from lists where id = $1"
+    sql = <<~HEREDOC
+      select lists.*, 
+        count(todos.id) as todos_count,
+        count(nullif(todos.completed, true)) as todos_remaining_count 
+      from lists
+      left join todos 
+      on todos.list_id = lists.id 
+      where lists.id = $1
+      group by lists.id
+      order by lists.name;
+    HEREDOC
+
     result = query(sql, id)
-    
-    tuple = result.first
-    
-    list_id = tuple['id'].to_i
-    todos = find_todos_for_list(list_id)
-    { id: list_id, name: tuple['name'], todos: todos }
+    tuple_to_list_hash(result.first)
   end
 
   def all_lists
-    sql = "select * from lists"
+    sql = <<~HEREDOC
+      select lists.*, 
+        count(todos.id) as todos_count,
+        count(nullif(todos.completed, true)) as todos_remaining_count 
+      from lists
+      left join todos 
+      on todos.list_id = lists.id 
+      group by lists.id
+      order by lists.name;
+    HEREDOC
+
     result = query(sql)
 
     result.map do |tuple|
-      list_id = tuple['id'].to_i
-      todos = find_todos_for_list(list_id)
-
-      { id: list_id, name: tuple['name'], todos: todos }
+      tuple_to_list_hash(tuple)
     end
   end
 
   def create_new_list(list_name)
     sql = "insert into lists (name) values ($1)"
     query(sql, list_name)
-    # @session[:lists] << { id: id, name: list_name, todos: []}
   end
 
   def delete_list(id)
     sql = "delete from lists where id = $1"
     query(sql, id)
-    # @session[:lists].reject! { |list| list[:id] == id }
   end
 
   def update_list_name(id, new_name)
     sql = "update lists set name = $1 where id = $2"
     query(sql, new_name, id)
-    # list = find_list(id)
-    # list[:name] = new_name
   end
 
   def create_new_todo(list_id, todo_name)
@@ -87,8 +96,6 @@ class DatabasePersistence
   #   @db.close
   # end
 
-  private
-
   def find_todos_for_list(list_id)
     todos_sql = "select * from todos where list_id = $1"
     todos_result = query(todos_sql, list_id)
@@ -99,5 +106,15 @@ class DatabasePersistence
         completed: todo_tuple['completed'] == 't'
       }
     end
+  end
+
+  private
+
+  def tuple_to_list_hash(tuple)
+    { id: tuple['id'].to_i,
+      name: tuple['name'], 
+      todos_count: tuple['todos_count'].to_i, 
+      todos_remaining_count: tuple['todos_remaining_count'].to_i
+    }
   end
 end
